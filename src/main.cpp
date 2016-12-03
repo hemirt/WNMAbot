95cab6796d94b90828d9acc78c94f9f1823ff5ef
Rasmus Karlsson <pajlada@bithack.se>
2016-12-03 09:36:53 +0000

    print error and usage if the user does not pass through required arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include "connhandler.hpp"
 
+#include <iostream>
+
 int
 main(int argc, char *argv[])
 {
+    // Make sure the user is passing through the required arguments
+    if (argc < 3) {
+        std::cerr << "usage: ./WNMAbot oauth username" << std::endl;
+        return 1;
+    }
+
     ConnHandler irc(argv[1], argv[2]);
 
     irc.joinChannel("pajlada");
