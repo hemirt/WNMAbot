8370feb57bbaf6475ba784ecdc41637725fefc4c
Hemirt <hemirt@email.cz>
2017-01-28 13:55:23 +0000

    remindusers files

---
+++ include/remindusers.hpp
@@ -0,0 +1,37 @@
+#ifndef REMINDUSERS_HPP
+#define REMINDUSERS_HPP
+
+#include <boost/asio.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <string>
+#include <utility>
+
+class RemindUsers
+{
+public:
+    struct Reminder {
+        std::string toUser;
+        std::string whichReminder;
+        boost::asio::steady_timer *timer;
+    };
+    RemindUsers() = default;
+    void addReminder(const std::string &fromUser, const std::string &toUser,
+                     const std::string &which,
+                     boost::asio::steady_timer *timer);
+    int count(const std::string &fromUser);
+    void removeFirst(const std::string &fromUser);
+    RemindUsers::Reminder getFirst(const std::string &fromUser);
+    void remove(const std::string &fromUser, const std::string &toUser,
+                const std::string &which);
+    bool exists(const std::string &fromUser, const std::string &toUser,
+                const std::string &which);
+
+private:
+    std::multimap<std::string, Reminder> reminders;
+    std::mutex mtx;
+};
+
+#endif
---
+++ src/remindusers.cpp
@@ -0,0 +1,75 @@
+#include "remindusers.hpp"
+
+void
+RemindUsers::addReminder(const std::string &fromUser, const std::string &toUser,
+                         const std::string &which,
+                         boost::asio::steady_timer *timer)
+{
+    std::lock_guard<std::mutex> lk(this->mtx);
+    this->reminders.emplace(std::pair<std::string, RemindUsers::Reminder>(
+        fromUser, {toUser, which, timer}));
+    return;
+}
+
+int
+RemindUsers::count(const std::string &fromUser)
+{
+    std::lock_guard<std::mutex> lk(this->mtx);
+    return this->reminders.count(fromUser);
+}
+
+void
+RemindUsers::removeFirst(const std::string &fromUser)
+{
+    std::lock_guard<std::mutex> lk(this->mtx);
+    if (this->reminders.count(fromUser)) {
+        auto range = this->reminders.equal_range(fromUser);
+        this->reminders.erase(range.first);
+    }
+}
+
+RemindUsers::Reminder
+RemindUsers::getFirst(const std::string &fromUser)
+{
+    Reminder retValue;
+    std::lock_guard<std::mutex> lk(this->mtx);
+    if (this->reminders.count(fromUser)) {
+        auto range = this->reminders.equal_range(fromUser);
+        retValue = range.first->second;
+    }
+    return retValue;
+}
+
+void
+RemindUsers::remove(const std::string &fromUser, const std::string &toUser,
+                    const std::string &which)
+{
+    std::lock_guard<std::mutex> lk(this->mtx);
+    if (this->reminders.count(fromUser)) {
+        auto range = this->reminders.equal_range(fromUser);
+        for (auto it = range.first; it != range.second; ++it) {
+            if (it->first == fromUser && it->second.toUser == toUser &&
+                it->second.whichReminder == which) {
+                this->reminders.erase(it);
+                return;
+            }
+        }
+    }
+}
+
+bool
+RemindUsers::exists(const std::string &fromUser, const std::string &toUser,
+                    const std::string &which)
+{
+    std::lock_guard<std::mutex> lk(this->mtx);
+    if (this->reminders.count(fromUser)) {
+        auto range = this->reminders.equal_range(fromUser);
+        for (auto it = range.first; it != range.second; ++it) {
+            if (it->first == fromUser && it->second.toUser == toUser &&
+                it->second.whichReminder == which) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
